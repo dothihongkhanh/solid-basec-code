04a59405b17d08af854cc6f5df544d886d9fb4cd
dothihongkhanh <dothihongkhanh22@email.com>
2024-04-19 17:09:55 +0700

    add login register

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
 use App\Services\User\CreateUserService;
 use App\Services\User\DeleteUserService;
 use App\Services\User\GetUserService;
 use App\Services\User\UpdateUserService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -33,5 +37,32 @@ class UserController extends Controller
     {
         $user = resolve(UpdateUserService::class)->setParams($request)->handle();
         return redirect()->route('users.index');
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $validatedData = $request->validated();
+        $validatedData['password'] = Hash::make($validatedData['password']);
+        $createUserService = resolve(CreateUserService::class);
+        $user = $createUserService->setParams($validatedData)->handle();
+
+        if ($user) {
+            return response()->json(['user' => $user], 201);
+        } else {
+            return response()->json(['error' => 'Registration failed'], 500);
+        }
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            
+            return response()->json(['user' => $user, 'message' => 'Login success'], 201);
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Payment\PaymentController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::POST('payment', [PaymentController::class, 'payment']);
+Route::POST('login', [UserController::class, 'login']);
+Route::POST('register', [UserController::class, 'register']);
